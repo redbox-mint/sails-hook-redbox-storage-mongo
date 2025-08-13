4074b5b13c4be4346634ca128e602c0bf0bc2176
Andrew Brazzatti <a.brazzatti@qcif.edu.au>
2025-08-13 04:38:24 +0000

    fix: comment out redundant npm install to prevent CI breaks and streamline mocha execution

--- support/run-mocha.sh
+++ support/run-mocha.sh
@@ -9,10 +9,16 @@ cd "${BASE_PATH}"
 npm install --ignore-scripts
 
 cd /opt/redbox-portal
-# This is breaking CI but we also shouldn't need to run the install again on a built base image so commenting it out
-# npm install --ignore-scripts
+# This is breaks in CI because we're volume mounting in the hook module and npm will try and change it. This works around the issue by doing the install in a tmp dir before copying everything but the hook back
 npm install -g mocha
-
+mkdir -p /tmp/redbox-portal
+cp -Rf node_modules /tmp/redbox-portal/
+cp package.json /tmp/redbox-portal/
+cp package-lock.json /tmp/redbox-portal/
+cd /tmp/redbox-portal && npm install --ignore-scripts
+rm -Rf /tmp/redbox-portal/node_modules/@researchdatabox/sails-hook-redbox-storage-mongo
+cp -Rf /tmp/redbox-portal/* /opt/redbox-portal/
+cd /opt/redbox-portal
 
 exec mocha
   --exit --no-package \
