b44ca47ca95bde7315e31243ef2c3042611a8c3a
Andrew Brazzatti <a.brazzatti@qcif.edu.au>
2025-08-13 04:19:11 +0000

    fix: update run-mocha.sh to install mocha globally and streamline execution

--- support/run-mocha.sh
+++ support/run-mocha.sh
@@ -11,9 +11,10 @@ npm install --ignore-scripts
 cd /opt/redbox-portal
 # This is breaking CI but we also shouldn't need to run the install again on a built base image so commenting it out
 # npm install --ignore-scripts
+npm install -g mocha
 
-exec node \
-  /opt/redbox-portal/node_modules/.bin/mocha \
+
+exec mocha
   --exit --no-package \
   --config ${BASE_PATH}/test/unit/.mocharc.js \
   ${BASE_PATH}/test/unit/bootstrap.js \
